8824a4cc43dd9fe68769ab719392d462452a19ef
yigit-guven <yigitguven0124@gmail.com>
2025-05-27 23:38:40 +0300

    Added Golden Wheat and Petting creative tab, Changed package name.

---
+++ forge-1.20.1/src/main/java/com/anchorstudios/petting/Petting.java
@@ -0,0 +1,86 @@
+package com.anchorstudios.petting;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import org.slf4j.Logger;
+
+@Mod(Petting.MODID)
+public class Petting {
+    public static final String MODID = "petting";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
+
+    public static final RegistryObject<Item> GOLDEN_WHEAT = ITEMS.register("golden_wheat",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<CreativeModeTab> PETTING_TAB = CREATIVE_MODE_TABS.register("petting_tab",
+            () -> CreativeModeTab.builder()
+                    .title(Component.translatable("itemGroup.petting_tab"))
+                    .icon(() -> new ItemStack(GOLDEN_WHEAT.get()))
+                    .displayItems((params, output) -> {
+                        output.accept(GOLDEN_WHEAT.get());
+                    })
+                    .build());
+
+    public Petting(FMLJavaModLoadingContext context) {
+        IEventBus modEventBus = context.getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        BLOCKS.register(modEventBus);
+        ITEMS.register(modEventBus);
+        CREATIVE_MODE_TABS.register(modEventBus);
+
+        MinecraftForge.EVENT_BUS.register(this);
+        modEventBus.addListener(this::addCreative);
+
+        context.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        LOGGER.info("Petting mod initialized!");
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+
+
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+        LOGGER.info("Petting mod server starting");
+    }
+
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+            LOGGER.info("Petting mod client setup");
+        }
+    }
+}
